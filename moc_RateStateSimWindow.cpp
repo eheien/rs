21c9c6bd10b1b5caa4cc512c0013d710bd81dded
Eric Heien <emheien@ucdavis.edu>
2015-02-03 10:17:28 -0800

    Snapshot of current development

--- moc_RateStateSimWindow.cpp
+++ moc_RateStateSimWindow.cpp
@@ -1,8 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'RateStateSimWindow.h'
 **
-** Created: Mon Apr 23 13:51:39 2012
-**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.6)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +9,8 @@
 #include "RateStateSimWindow.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'RateStateSimWindow.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 62
-#error "This file was generated using the moc from 4.7.4. It"
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.6. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,7 +19,7 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_RateStateSimWindow[] = {
 
  // content:
-       5,       // revision
+       6,       // revision
        0,       // classname
        0,    0, // classinfo
        5,   14, // methods
@@ -31,26 +30,45 @@ static const uint qt_meta_data_RateStateSimWindow[] = {
        0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
-      28,   20,   19,   19, 0x0a,
-      52,   20,   19,   19, 0x0a,
-      77,   20,   19,   19, 0x0a,
-     101,   20,   19,   19, 0x0a,
-     125,   19,   19,   19, 0x0a,
+      19,   43,   51,   51, 0x0a,
+      52,   43,   51,   51, 0x0a,
+      77,   43,   51,   51, 0x0a,
+     101,   43,   51,   51, 0x0a,
+     125,   51,   51,   51, 0x0a,
 
        0        // eod
 };
 
 static const char qt_meta_stringdata_RateStateSimWindow[] = {
-    "RateStateSimWindow\0\0new_val\0"
-    "a_param_changed(double)\0"
-    "ab_ratio_changed(double)\0"
+    "RateStateSimWindow\0a_param_changed(double)\0"
+    "new_val\0\0ab_ratio_changed(double)\0"
     "k_param_changed(double)\0r_param_changed(double)\0"
     "recalc()\0"
 };
 
+void RateStateSimWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        RateStateSimWindow *_t = static_cast<RateStateSimWindow *>(_o);
+        switch (_id) {
+        case 0: _t->a_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
+        case 1: _t->ab_ratio_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
+        case 2: _t->k_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
+        case 3: _t->r_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
+        case 4: _t->recalc(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObjectExtraData RateStateSimWindow::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
 const QMetaObject RateStateSimWindow::staticMetaObject = {
     { &QMainWindow::staticMetaObject, qt_meta_stringdata_RateStateSimWindow,
-      qt_meta_data_RateStateSimWindow, 0 }
+      qt_meta_data_RateStateSimWindow, &staticMetaObjectExtraData }
 };
 
 #ifdef Q_NO_DATA_RELOCATION
@@ -76,14 +94,8 @@ int RateStateSimWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        switch (_id) {
-        case 0: a_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
-        case 1: ab_ratio_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
-        case 2: k_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
-        case 3: r_param_changed((*reinterpret_cast< const double(*)>(_a[1]))); break;
-        case 4: recalc(); break;
-        default: ;
-        }
+        if (_id < 5)
+            qt_static_metacall(this, _c, _id, _a);
         _id -= 5;
     }
     return _id;
